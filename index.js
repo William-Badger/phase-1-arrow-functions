7d70bf02bf0e19a15af3f7a41b6ff612892e013a
William Badger <wkbadger@gmail.com>
2021-06-09 14:25:56 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const divide = (num1 = 2000,num2 = 100) => {
+    return num1/num2;
+}
+
+let x = 2;
+const square = (x) => { return x*x;}
+
+let a = 3;
+let b = 4;
+const add = (a,b) => { return a + b }
